b0416b7043d9540d5b08e411fb66567928008258
Emamul Andalib <emamul.andalib@gmail.com>
2018-06-06 15:22:15 +0600

    - logic fixed as per the document

--- project-1/app.js
+++ project-1/app.js
@@ -1,32 +1,48 @@
 const Animation = {
+    position: {
+        x: 0,
+        y: 0
+    },
+    direction: {
+        x: 1,
+        y: 1
+    },
+    selectorDimension: 0,
+
     getWidth: () => document.querySelector('body').offsetWidth,
     getHeight: () => document.querySelector('body').offsetHeight,
-    goUpDown: true,
-    goLeftRight: false,
-    corner: 1,
 
     startAnimation(selector) {
         setInterval(() => {
-            if (this.goUpDown) {
-                selector.style.top = (this.corner === 3) ? 0 : this.getHeight() - 100
-                this.goLeftRight = true
-                this.goUpDown = false
-                this.corner += 1
-            } else {
-                selector.style.left = (this.corner === 4) ? 0 : this.getWidth() - 100
-                this.goUpDown = true
-                this.goLeftRight = false
-                this.corner += 1
+            this.position.x += (10 * this.direction.x);
+            this.position.y += (10 * this.direction.y);
+
+            if (this.position.x > this.getWidth() - this.selectorDimension) {
+                this.direction.x *= -1;
+                this.position.x = this.getWidth() - this.selectorDimension;
+            } else if (this.position.x < 0) {
+                this.direction.x *= -1;
+                this.position.x = 0;
             }
 
-            if (this.corner > 4) this.corner = 1;
-        }, 1000)
+            if (this.position.y > this.getHeight() - this.selectorDimension) {
+                this.direction.y *= -1;
+                this.position.y = this.getHeight() - this.selectorDimension;
+            } else if (this.position.y < 0) {
+                this.direction.y *= -1;
+                this.position.y = 0;
+            }
+            
+            selector.style.left = this.position.x + 'px';
+            selector.style.top = this.position.y + 'px';
+        }, 100)
     },
 
     animateTheBox(selector) {
         if (selector) {
             selector.style.position = "absolute";
             selector.style.top = "50px";
+            this.selectorDimension = selector.offsetWidth;
             this.startAnimation(selector)
         }
     }
